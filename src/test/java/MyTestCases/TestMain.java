d7f51bcdd277ef6072bd02f73cc609b0cbe76959
Jeyaprakash-Rajendran <rjprakash202@gmail.com>
2021-07-18 10:47:45 +0530

    firstcommit

---
+++ src/test/java/MyTestCases/TestMain.java
@@ -0,0 +1,18 @@
+package MyTestCases;
+
+import org.testng.annotations.Test;
+
+public class TestMain {
+	
+	@Test
+	public void testHelloworld() {
+		System.out.println("cool");
+	}
+
+	@Test
+	public void testHelloworld1() {
+		System.out.println("cool1");
+	}
+
+
+}
