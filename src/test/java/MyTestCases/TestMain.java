6fe1aac7be7f64483a62418db57f31235a002f85
Jeyaprakash-Rajendran <rjprakash202@gmail.com>
2021-07-18 11:53:07 +0530

    mylatestchanges

--- src/test/java/MyTestCases/TestMain.java
+++ src/test/java/MyTestCases/TestMain.java
@@ -1,18 +1,26 @@
 package MyTestCases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestMain {
 	
 	@Test
 	public void testHelloworld() {
-		System.out.println("cool");
+		System.setProperty("webdriver.chrome.driver","C:\\Automation\\Drivers\\chromedriver_win32\\C-90\\chromedriver.exe");  
+	    WebDriver driver=new ChromeDriver();
+	    String driverPath = "";
+	     driver.get("https://www.google.com");
+		System.out.println("test1");
 	}
 
 	@Test
 	public void testHelloworld1() {
-		System.out.println("cool1");
-	}
+		System.setProperty("webdriver.chrome.driver","C:\\Automation\\Drivers\\chromedriver_win32\\C-90\\chromedriver.exe");  
+	    WebDriver driver1=new ChromeDriver();
+	    driver1.get("https://www.facebook.com");
+		System.out.println("test2");	}
 
 
 }
